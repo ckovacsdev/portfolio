662d089d260753dc41ba36352d8289fff09949ae
ckovacsdev <ckovacsdev@gmail.com>
2025-01-20 16:12:32 -0500

    menu mobile responsiveness

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
 import LinkedinIcon from '../../assets/linkedinicon.png';
 import GithubIcon from '../../assets/githubicon.png'
 import './header.css';
+import { useState } from 'react';
+import Hamburger from 'hamburger-react';
 
 const LinkedInUrl = 'https://www.linkedin.com/in/christian-kovacs-23198b1b0/';
 const GitHubUrl = 'https://github.com/ckovacsdev';
 
 export const Header = () => {
+
+    const [ isOpen, setOpen ] = useState(false);
+
     return (
         <div className='header-container'>
             <div className='header-section-container section-name'>
@@ -25,7 +29,13 @@ export const Header = () => {
                 <div className='header-icons'> 
                     <a href={LinkedInUrl} target='_blank' rel='noopener noreferrer'> <img src={LinkedinIcon} alt="LinkedIn Icon with Link" className='header-icon'/> </a>
                     <a href={GitHubUrl} target='_blank' rel='noopener noreferrer'> <img src={GithubIcon} alt="GitHub Icon with Link" className='header-icon github-icon'/> </a>
+                    <div className='mobile-menu'>
+                        <Hamburger toggled={isOpen} toggle={setOpen} size={40} color='white'/>
+                    </div>
                 </div>
+                
+
+
             </div>
         </div>
     )
