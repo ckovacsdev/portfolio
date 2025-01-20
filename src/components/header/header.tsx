4448d360ce847a545ce9432e7728002229e55a3e
ckovacsdev <ckovacsdev@gmail.com>
2025-01-19 21:14:49 -0500

    add some header content

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import LinkedinIcon from '../../assets/linkedinicon.png';
+import GithubIcon from '../../assets/githubicon.png'
 import './header.css';
 
 export const Header = () => {
     return (
-        <></>
+        <div className='header-container'>
+            <div className='header-name-container'>
+                <h1 className='header-name'> Christian Kovacs </h1>
+            </div>
+
+            <div className='header-menu'>
+                <div className='header-menu-links'>
+                    <a href='#'> Projects </a>
+                    <a href='#'> Work History </a>
+                    <a href='#'> About Me </a>
+                </div>
+            </div>
+
+            <div className='header-links-container'>
+                <div className='header-links'> 
+                    {/* TODO
+                        Linkedin Icon linking to my page
+                        GitHub Icon linking to my page
+                    */}
+                    <img src={LinkedinIcon} alt="LinkedIn Icon with Link" className='header-icon'/>
+                    <img src={GithubIcon} alt="GitHub Icon with Link" className='github-icon'/>
+
+                </div>
+            </div>
+        </div>
     )
 }
