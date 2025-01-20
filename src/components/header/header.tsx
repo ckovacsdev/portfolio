86c93489d73c43cf1150dc1ff9ca787cb2e104f7
ckovacsdev <ckovacsdev@gmail.com>
2025-01-19 21:25:06 -0500

    add some spacing to right side links

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -23,13 +23,8 @@ export const Header = () => {
 
             <div className='header-section-container'>
                 <div className='header-links'> 
-                    {/* TODO
-                        Linkedin Icon linking to my page
-                        GitHub Icon linking to my page
-                    */}
                     <a href={LinkedInUrl} target='_blank' rel='noopener noreferrer'> <img src={LinkedinIcon} alt="LinkedIn Icon with Link" className='header-icon'/> </a>
                     <a href={GitHubUrl} target='_blank' rel='noopener noreferrer'> <img src={GithubIcon} alt="GitHub Icon with Link" className='header-icon github-icon'/> </a>
-
                 </div>
             </div>
         </div>
