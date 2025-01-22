e2d3c9ef8290aca86ccc387821409afd600b6048
ckovacsdev <ckovacsdev@gmail.com>
2025-01-21 22:18:36 -0500

    add button styling + dropdown

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import LinkedinIcon from '../../assets/linkedinicon.png';
-import GithubIcon from '../../assets/githubicon.png'
+import GithubIcon from '../../assets/githubicon.png';
+import { AiOutlineMail } from "react-icons/ai";
 import './header.css';
 import { useEffect, useState } from 'react';
 import Hamburger from 'hamburger-react';
@@ -45,8 +46,18 @@ export const Header = () => {
                 
                 <div className='header-section-container section-icons'>
                     <div className='header-icons'> 
-                        <a href={LinkedInUrl} target='_blank' rel='noopener noreferrer'> <img src={LinkedinIcon} alt="LinkedIn Icon with Link" className='header-icon'/> </a>
-                        <a href={GitHubUrl} target='_blank' rel='noopener noreferrer'> <img src={GithubIcon} alt="GitHub Icon with Link" className='header-icon github-icon'/> </a>
+                        <a href={LinkedInUrl} target='_blank' rel='noopener noreferrer'> 
+                            <img src={LinkedinIcon} alt="LinkedIn Icon with Link" className='header-icon'/> 
+                        </a>
+                        <a href={GitHubUrl} target='_blank' rel='noopener noreferrer'> 
+                            <img src={GithubIcon} alt="GitHub Icon with Link" className='header-icon github-icon'/> 
+                        </a>
+                        
+                        <button className='header-email-button'>
+                            <AiOutlineMail color={'white'} size={'40px'} />
+                        </button>
+                 
+
                         <div className='mobile-menu'>
                             <Hamburger toggled={isOpen} toggle={setOpen} size={40} color='white'/>
                         </div>
@@ -69,6 +80,11 @@ export const Header = () => {
                         <a href={GitHubUrl} target='_blank' rel='noopener noreferrer' className='dropdown-icon-container'> 
                             <img src={GithubIcon} alt="GitHub Icon with Link" className='dropdown-icon gitlab-dropdown'/> 
                         </a>
+
+                        <button className='dropdown-icon-container email-button-sub'> 
+                            <AiOutlineMail color={'white'} size={'40px'} />
+                        </button>
+                        
                     </div>
 
                 </div>
