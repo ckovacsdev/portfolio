a70676ee0a6e6596662a5bd6d83115f38d02a335
ckovacsdev <ckovacsdev@gmail.com>
2025-01-21 21:56:44 -0500

    work history base component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/header/header';
 import { ProfileCard } from './components/profile-card/profile-card';
+import { WorkHistory } from './components/work-history-card/work-history';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div className='main-profile-content'>
         <ProfileCard />
+        <WorkHistory />
       </div>
     </div>
   );
---
+++ src/components/work-history-card/work-history.tsx
@@ -0,0 +1,11 @@
+import './work-history.css';
+
+export const WorkHistory = () => {
+    return(
+        <div className='work-history-container'>
+            <div className='work-history-section'>
+                
+            </div>
+        </div>
+    )
+}
