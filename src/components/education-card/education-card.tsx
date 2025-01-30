03e7128ed4e3beea9306a846b324a7fddcdc0eca
ckovacsdev <ckovacsdev@gmail.com>
2025-01-29 22:28:32 -0500

    edu card conatiner

--- src/components/education-card/education-card.tsx
+++ src/components/education-card/education-card.tsx
@@ -2,6 +2,8 @@ import './education-card.css';
 
 export const EducationCard = () => {
     return (
-        <></>
+        <div className='education-card-container'>
+            
+        </div>
     )
 }
