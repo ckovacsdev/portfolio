bae9f160e298d5fb96104bedef4a618c7df53835
Christian Kovacs <ckovacsdev@gmail.com>
2025-04-16 22:21:51 -0400

    add docker logo to profile card

--- src/components/profile-card/profile-card.tsx
+++ src/components/profile-card/profile-card.tsx
@@ -1,6 +1,7 @@
 import { PiMapPinSimple } from "react-icons/pi";
 import { SiTypescript } from "react-icons/si";
 import { FaReact } from "react-icons/fa";
+import { FaDocker } from 'react-icons/fa';
 import { FaGolang } from "react-icons/fa6";
 import { SiGraphql } from "react-icons/si";
 import { AiOutlineDotNet } from "react-icons/ai";
@@ -29,6 +30,7 @@ export const ProfileCard = () => {
                         <div className='profile-icon-name-pair'> <SiGraphql size={'25px'} color={'#E10098'} /> GraphQL </div>
                         <div className='profile-icon-name-pair'> <AiOutlineDotNet size={'40'} color={'#512bd4'} /> .NET Core </div>
                         <div className='profile-icon-name-pair'> <FaGolang size={'40px'} color={'#29BEB0'} /> Go </div>
+                        <div className='profile-icon-name-pair'> <FaDocker size={'25px'} color={'#1D63ED'} /> Docker </div>
                     </div>
                 </div>
            </div>
